5ba02eacd2a509f7db1b9904b01e7b231fe8404c
Eric Anderson <anderson.eric.joseph@gmail.com>
2026-04-04 06:56:39 -0600

    set Warrior Shield Bash rank 2 and 3 to 'wait' instead of 'yes'

--- data/abilities/warrior.lua
+++ data/abilities/warrior.lua
@@ -160,8 +160,8 @@ Deathless.Data.Abilities["Warrior"] = {
 
     -- Shield Bash
     { name = "Shield Bash", level = 12, rank = 1, base_cost = 1000, icon = "Ability_Warrior_ShieldBash", source = "trainer", train = "yes", spellId = 72 },
-    { name = "Shield Bash", level = 32, rank = 2, base_cost = 14000, icon = "Ability_Warrior_ShieldBash", source = "trainer", train = "yes", spellId = 1671 },
-    { name = "Shield Bash", level = 52, rank = 3, base_cost = 54000, icon = "Ability_Warrior_ShieldBash", source = "trainer", train = "yes", spellId = 1672 },
+    { name = "Shield Bash", level = 32, rank = 2, base_cost = 14000, icon = "Ability_Warrior_ShieldBash", source = "trainer", train = "wait", spellId = 1671 },
+    { name = "Shield Bash", level = 52, rank = 3, base_cost = 54000, icon = "Ability_Warrior_ShieldBash", source = "trainer", train = "wait", spellId = 1672 },
 
     -- Shield Block
     { name = "Shield Block", level = 16, rank = 1, base_cost = 2000, icon = "Ability_Defend", source = "trainer", train = "yes", spellId = 2565 },
